d445c9178c5586c87bfaea3e3d552786b51358fa
Elijah Gjerdingen <elijah.gjerdingen@gmail.com>
2020-10-31 17:16:23 -0500

    Project creation and changes from 10/31
    First day working on project

---
+++ Esports/Esports.API/Controllers/playerscontroller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Esports.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class playerscontroller : ControllerBase
+    {
+        [HttpGet]
+        public string GetPlayers() { return "Bikerthulu"; }
+    }
+}
