ad1e91d12ea32e58804fe337e804722f9acc0b72
Elijah Gjerdingen <elijah.gjerdingen@gmail.com>
2020-11-15 15:02:30 -0600

    Commiting work from 11/15
    Adding querying functionality and sorting to searches

---
+++ Esports/Esports.API/Classes/PlayerQueryParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esports.API.Classes
+{
+    public class PlayerQueryParameters : QueryParameters
+    {
+        public string GamerTag { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Esports/Esports.API/Controllers/playerscontroller.cs
+++ Esports/Esports.API/Controllers/playerscontroller.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Esport.API.Models;
+using Esports.API.Classes;
 using Esports.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Esports.API.Controllers
 {
@@ -23,6 +26,49 @@ namespace Esports.API.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Player> GetAllPlayers() { return _context.Players.ToArray(); }
+        public async Task<IActionResult> GetAllPlayers([FromQuery] PlayerQueryParameters queryParameters) 
+        {
+            IQueryable<Player> player = _context.Players;
+
+            if(queryParameters.MinPrice != null && queryParameters.MaxPrice != null)
+            {
+                player = player.Where(
+                    p => p.Price >= queryParameters.MinPrice.Value && p.Price <= queryParameters.MaxPrice.Value
+                    );
+            }
+            if(!string.IsNullOrEmpty(queryParameters.GamerTag))
+            {
+                player = player.Where(p => p.GamerTag == queryParameters.GamerTag);
+            }
+            if(!string.IsNullOrEmpty(queryParameters.Name))
+            {
+                player = player.Where(p => p.Name.ToLower().Contains(queryParameters.Name.ToLower()));
+            }
+
+            if(!string.IsNullOrEmpty(queryParameters.SortBy))
+            {
+                if(typeof(Player).GetProperty(queryParameters.SortBy) != null)
+                {
+                    player = player.OrderByCustom(queryParameters.SortBy, queryParameters.SortOrder);
+                }
+            }
+
+            player = player
+                .Skip(queryParameters.Size * (queryParameters.Page - 1))
+                .Take(queryParameters.Size);
+
+            return Ok(await player.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPlayer(int id)
+        {
+            var player = await _context.Players.FindAsync(id);
+            if(player == null)
+            {
+                return NotFound();
+            }
+            return Ok(player);
+        }
     }
 }
